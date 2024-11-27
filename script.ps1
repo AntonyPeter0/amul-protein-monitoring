c587ceb9610a42b8f0a8d1a34d62bb5c69789628
AntonyPeter0 <antonypeter1998@gmail.com>
2024-11-28 00:14:58 +0530

    more changes

--- script.ps1
+++ script.ps1
@@ -5,6 +5,18 @@ $webSession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $putUrl = "https://shop.amul.com/entity/ms.settings/_/setPreferences"
 $getUrl = "https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0"
 
+
+$From       = 'Antony'
+$To         = $env:TO_USERNAME
+$Subject    = 'Amul Product is Back in Stock'
+$Body       = 'Urgent Please Buy'
+$SmtpServer = 'smtp.gmail.com'
+$Port       = 587
+$UseSsl     = $true
+$Credential = New-Object pscredential $env:MAIL_USERNAME,$($env:MAIL_PASSWORD |ConvertTo-SecureString -AsPlainText -Force)
+
+
+
 # Define the payload for the PUT request
 $putPayload = @{
     data = @{
@@ -35,14 +47,5 @@ if ($responseGet) {
     Write-Output "GET request failed."
 }
 
-$MailArgs = @{
-    From       = 'Antony'
-    To         = '$env:TO_USERNAME'
-    Subject    = 'Amul Product is Back in Stock'
-    Body       = 'Urgent Please Buy'
-    SmtpServer = 'smtp.gmail.com'
-    Port       = 587
-    UseSsl     = $true
-    Credential = New-Object pscredential '$env:MAIL_USERNAME',$('$env:MAIL_PASSWORD' |ConvertTo-SecureString -AsPlainText -Force)
-}
-Send-MailMessage @MailArgs
+
+Send-MailMessage -To $To -From $From -Subject $Subject -Body $Body -Credential $Credential -SmtpServer $SmtpServer -Port $Port -UseSsl $UseSsl
